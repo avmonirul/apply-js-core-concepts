e5fc0003451fb100a121d3f7de53134130e36a7d
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 15:32:13 +0600

    first commit

--- inctofet.js
+++ inctofet.js
@@ -1,11 +1,26 @@
-const myInches =12;
-const myFeet = myInches/12;
-console.log(myFeet);
+// const myInches =12;
+// const myFeet = myInches/12;
+// console.log(myFeet);
+
+// const dadaInches = 144;
+// const dadaFeet = dadaInches / 12;
+// console.log('dada feet', dadaFeet);
+
+// const dadiInches = 60;
+// const dadiFeet = dadiInches /12;
+// console.log('dadi feet', dadiFeet)
+
+
+function inchToFeet(inches){
+    const feet = inches / 12;
+    return feet;
+
+}
 
 const dadaInches = 144;
-const dadaFeet = dadaInches / 12;
-console.log('dada feet', dadaFeet);
+const dadaFeet = inchToFeet(dadaInches);
+console.log(dadaFeet)
 
-const dadiInches = 60;
-const dadiFeet = dadiInches /12;
-console.log('dadi feet', dadiFeet)
+const nanaInches = 168;
+const nanaFeet = inchToFeet(nanaInches);
+console.log('naan feet', nanaFeet)
