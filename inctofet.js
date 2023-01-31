c3af76164f4f63d049be3a3edffbc9b7a3efef5d
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 15:12:02 +0600

    first commit

--- inctofet.js
+++ inctofet.js
@@ -5,3 +5,7 @@ console.log(myFeet);
 const dadaInches = 144;
 const dadaFeet = dadaInches / 12;
 console.log('dada feet', dadaFeet);
+
+const dadiInches = 60;
+const dadiFeet = dadiInches /12;
+console.log('dadi feet', dadiFeet)
