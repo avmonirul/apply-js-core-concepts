a2025ad4cd0fe23a2bc9796ec66f3fda38ac22a2
MONIRUL ISLAM <monirul0ads@gmail.com>
2023-01-31 14:48:48 +0600

    first commit

---
+++ recap.js
@@ -0,0 +1,48 @@
+// 1. variable 
+var myNamer = 'khan';
+
+//math operation: +, -, * /
+//shorthand: +=, --, *=, /=
+// ++, -- mining
+
+
+// 2. array
+var friends = ['abul','babul', 'cabul', 'dadul', 'ebul'];
+
+var thirdFriends = friends[2];
+friends[3] = 'dambool';
+
+//3. conditional
+// >, <, <=, ==, ===, !=, !==
+
+if(friends.length <2){
+    console.log('fokor tor kono friends ni')
+}
+else{
+
+}
+
+// 4. loop
+var number = 0;
+while(number <= 6){
+    console.log(number);
+    number++;
+}
+
+for(var i = 0; i <= 10; i++){
+
+}
+
+// 5. function
+function isMoonUp(time){
+    if(time > 7){
+        return true;
+    }
+}
+
+// 6. object
+var jantus = {
+    height:60,
+    romanticism:'havvy',
+    bapErtaka:'borolox'
+}
---
+++ var-let-const.js
@@ -0,0 +1,5 @@
+let age = 11;
+age =21;
+
+const countyName = 'bangladesh';
+console.log(age,countyName);
